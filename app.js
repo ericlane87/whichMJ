a3e837243d381ce0d97213e58e334b3ca0064dbf
Eric Lane <ericlane87@gmail.com>
2026-04-08 16:49:31 -0400

    Refine fighter outfits in Phaser portraits

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const FIGHTERS = {
   court: {
     id: "court",
     name: "Court MJ",
-    subtitle: "Bald power scorer",
+    subtitle: "No. 23 court scorer",
     color: 0xff7a18,
     accent: 0xffb100,
     skin: 0x4c2d1f,
@@ -20,7 +20,7 @@ const FIGHTERS = {
   stage: {
     id: "stage",
     name: "Stage MJ",
-    subtitle: "Long-haired rhythm striker",
+    subtitle: "Long-haired stage striker",
     color: 0x19c8bb,
     accent: 0x60a5fa,
     skin: 0xb77453,
@@ -114,40 +114,104 @@ class BootScene extends Phaser.Scene {
     g.fillStyle(0x07111a, 1);
     g.fillEllipse(110, 238, 148, 22);
 
-    g.fillStyle(fighter.skin, 1);
-    g.fillCircle(110, 54, fighter.id === "court" ? 24 : 22);
-    if (fighter.id === "stage") {
-      g.fillStyle(0x131313, 1);
-      g.fillEllipse(110, 44, 56, 34);
-      g.fillRect(82, 54, 12, 72);
-      g.fillRect(126, 54, 12, 72);
-    }
-
-    g.fillStyle(fighter.skin, 1);
-    g.fillRoundedRect(94, 76, 32, 40, 14);
-    g.fillRoundedRect(72, 88, 18, 72, 9);
-    g.fillRoundedRect(130, 84, 18, 72, 9);
-
-    g.fillStyle(fighter.color, 1);
-    g.fillRoundedRect(78, 96, 64, 60, 16);
-    g.fillStyle(0xf3f4f6, 1);
-    g.fillRect(88, 156, 18, 72);
-    g.fillRect(114, 156, 18, 72);
-    g.fillStyle(0xd6d6d6, 1);
-    g.fillRoundedRect(86, 224, 22, 10, 5);
-    g.fillRoundedRect(114, 224, 22, 10, 5);
-
     if (fighter.id === "court") {
-      g.fillStyle(fighter.accent, 1);
-      g.fillCircle(162, 132, 22);
-    } else {
-      g.lineStyle(8, fighter.accent, 1);
-      g.lineBetween(148, 108, 186, 96);
-      g.lineStyle(4, 0xddf7ff, 0.85);
-      g.lineBetween(156, 124, 194, 112);
-    }
+      g.fillStyle(fighter.skin, 1);
+      g.fillCircle(110, 52, 23);
+      g.fillRoundedRect(100, 74, 20, 28, 10);
+      g.fillRoundedRect(72, 94, 20, 74, 10);
+      g.fillRoundedRect(128, 94, 20, 74, 10);
 
-    g.generateTexture(key, 220, 260);
+      g.fillStyle(0x8f1d1d, 1);
+      g.beginPath();
+      g.moveTo(78, 88);
+      g.lineTo(142, 88);
+      g.lineTo(154, 158);
+      g.lineTo(66, 158);
+      g.closePath();
+      g.fillPath();
+      g.fillStyle(0xffffff, 1);
+      g.lineStyle(4, 0xffffff, 1);
+      g.strokeRoundedRect(76, 90, 68, 70, 18);
+      g.lineStyle();
+      g.fillRoundedRect(78, 156, 26, 74, 10);
+      g.fillRoundedRect(116, 156, 26, 74, 10);
+      g.fillStyle(0xd1d5db, 1);
+      g.fillRoundedRect(76, 224, 30, 12, 6);
+      g.fillRoundedRect(114, 224, 30, 12, 6);
+
+      const jersey = this.add.text(110, 125, "23", {
+        fontFamily: "Archivo Black",
+        fontSize: "30px",
+        color: "#ffffff"
+      }).setOrigin(0.5);
+      jersey.setDepth(10);
+      g.fillStyle(fighter.skin, 1);
+      g.fillRoundedRect(88, 162, 14, 34, 7);
+      g.fillRoundedRect(118, 162, 14, 34, 7);
+      g.fillStyle(fighter.accent, 1);
+      g.fillCircle(168, 128, 20);
+      g.lineStyle(3, 0xa34b00, 1);
+      g.strokeCircle(168, 128, 20);
+      g.lineBetween(148, 128, 188, 128);
+      g.lineBetween(168, 108, 168, 148);
+      g.lineBetween(156, 116, 180, 140);
+      g.lineBetween(180, 116, 156, 140);
+      g.generateTexture(key, 220, 260);
+      jersey.destroy();
+    } else {
+      g.fillStyle(fighter.skin, 1);
+      g.fillCircle(110, 54, 21);
+      g.fillStyle(0x111111, 1);
+      g.fillEllipse(110, 42, 54, 28);
+      g.fillRoundedRect(82, 46, 12, 82, 6);
+      g.fillRoundedRect(126, 46, 12, 82, 6);
+
+      g.fillStyle(fighter.skin, 1);
+      g.fillRoundedRect(98, 74, 24, 30, 10);
+      g.fillRoundedRect(74, 92, 16, 72, 8);
+      g.fillRoundedRect(130, 88, 16, 68, 8);
+
+      g.fillStyle(0x0f0f10, 1);
+      g.beginPath();
+      g.moveTo(82, 92);
+      g.lineTo(138, 92);
+      g.lineTo(150, 156);
+      g.lineTo(70, 156);
+      g.closePath();
+      g.fillPath();
+
+      g.fillStyle(0xeff1f4, 1);
+      g.beginPath();
+      g.moveTo(100, 94);
+      g.lineTo(120, 94);
+      g.lineTo(126, 156);
+      g.lineTo(94, 156);
+      g.closePath();
+      g.fillPath();
+
+      g.fillStyle(0xf7d046, 1);
+      g.fillRoundedRect(80, 98, 8, 50, 3);
+      g.fillRoundedRect(132, 98, 8, 50, 3);
+      g.fillRoundedRect(92, 90, 36, 8, 4);
+
+      g.fillStyle(0x0f0f10, 1);
+      g.fillRoundedRect(86, 156, 18, 72, 9);
+      g.fillRoundedRect(116, 152, 18, 76, 9);
+      g.fillStyle(0xffffff, 1);
+      g.fillRoundedRect(86, 212, 18, 12, 5);
+      g.fillRoundedRect(116, 212, 18, 12, 5);
+      g.fillStyle(0x0f0f10, 1);
+      g.fillRoundedRect(84, 224, 22, 8, 4);
+      g.fillRoundedRect(114, 224, 22, 8, 4);
+
+      g.lineStyle(8, fighter.accent, 1);
+      g.lineBetween(146, 110, 184, 98);
+      g.lineStyle(4, 0xddf7ff, 0.9);
+      g.lineBetween(154, 126, 192, 114);
+      g.lineStyle(5, 0xffffff, 0.9);
+      g.lineBetween(156, 140, 182, 164);
+      g.generateTexture(key, 220, 260);
+    }
     g.destroy();
   }
 
